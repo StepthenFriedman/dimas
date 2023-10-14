aa0f69d95b9456e327a7295e14997320ffcd846c
StepthenFriedman <143859543+StepthenFriedman@users.noreply.github.com>
2023-10-14 18:33:32 +0800

    10/14

--- 1237 - 2305-2306-第一次作业/E-2.c
+++ 1237 - 2305-2306-第一次作业/E-2.c
@@ -17,7 +17,9 @@ int main(){
 	int x=0,y=0,i=0,sevs=0;
 	while (scanf("%d %d",&x,&y)!=EOF){
 		sevs=0;
-		for (i=0;i<5;i++) sevs+=((y/tenth(i+1))-(x/tenth(i+1))+(digit(y,i)>7)+(digit(x,i)<=7)-1)*tenth(i)+(digit(y,i)==7)*((y%tenth(i))+1)-(digit(x,i)==7)*(x%tenth(i));
+		for (i=0;i<5;i++) sevs+=((y/tenth(i+1))-(x/tenth(i+1))+(digit(y,i)>7)+(digit(x,i)<=7)-1)*tenth(i)
+								+(digit(y,i)==7)*((y%tenth(i))+1)
+								-(digit(x,i)==7)*(x%tenth(i));
 		printf("%d\n",sevs);
 	}
 	return 0;
