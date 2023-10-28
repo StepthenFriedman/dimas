5f08bae7ce7b2176499a84e6fc5e9cee8d4d3cfa
StepthenFriedman <143859543+StepthenFriedman@users.noreply.github.com>
2023-10-28 11:17:48 +0800

    10/28

--- 作业与课堂练习/双节八天乐[2307-2308]/问题AF-22-循环-1-输出图形.c
+++ 作业与课堂练习/双节八天乐[2307-2308]/问题AF-22-循环-1-输出图形.c
@@ -3,9 +3,9 @@ int main(){
 	int i,j,k;
 	while (scanf("%d",&i)!=EOF){
 		for (j=0;j<i;j++){
-			for (k=1;k<j;k++) putchar(' ');
-			printf(65+k);
+			printf("%*d\n",j+1,j+1);
 		}
+		putchar('\n');
 	}
 	return 0;
 }
