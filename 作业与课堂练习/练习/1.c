4d485e28789a0ee3d7edb3e993962d152c55a44a
StepthenFriedman <143859543+StepthenFriedman@users.noreply.github.com>
2023-11-01 11:13:38 +0800

    11/1

--- 作业与课堂练习/练习/1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	float ipt;
-	while (scanf("%f",&ipt)!=EOF) printf("%.2f\n",(ipt<=110)?(ipt/2):(ipt<=210)?((ipt-110.)*0.55+55.):((ipt-210)*0.7+110.));
-	return 0;
-}
--- 作业与课堂练习/练习/2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	int a,b;
-	while (scanf("%d %d",&a,&b)!=EOF) printf("%d\n",(b-2)?((b>7)^(b%2))?31:30:(a%4)?28:29);
-	return 0;
-}
