2a51faacb4a0a9c4346986ca082e4cdca4445ded
StepthenFriedman <143859543+StepthenFriedman@users.noreply.github.com>
2023-11-01 14:54:57 +0800

    11/1

--- 作业与课堂练习/1247-2305-2306班-第二次OJ作业/问题A-开心的小明-有趣的游戏-保序数组-空间优化.c
+++ 作业与课堂练习/1247-2305-2306班-第二次OJ作业/问题A-开心的小明-有趣的游戏-保序数组-空间优化.c
@@ -1,29 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int cmp(const void *a,const void *b){
-	return *(int*)a-*(int*)b;
-}
+int cmp(const void *a,const void *b){return *(int*)a-*(int*)b;}
 
 int main(){
-	int n,i,j,k,sum;
+	int n,i,j=0,k=1,sum;
 	scanf("%d",&n);
 	int a[n];
 	for(i=0;i<n;i++) scanf("%d",&a[i]);
 	qsort(a,n,sizeof(int),cmp);
-	sum=a[0]=a[0]+a[1];
-	i=2;j=0;k=1;
+	sum=a[0]=a[0]+a[1];i=2;
 	while (i<n||j<k-1){
-		
-		if (a[j]<a[i]||i>=n) a[k]=a[j++];
-		else a[k]=a[i++];
-		
-		if (a[j]<a[i]||i>=n) a[k]+=a[j++];
-		else a[k]+=a[i++];
-		
-		sum+=a[k++];
+		a[k]=(a[j]<a[i]||i>=n)?a[j++]:a[i++];
+		sum+=a[k++]+=(a[j]<a[i]||i>=n)?a[j++]:a[i++];
 	}
-	
 	printf("%d\n",sum);
 	return 0;
 }
