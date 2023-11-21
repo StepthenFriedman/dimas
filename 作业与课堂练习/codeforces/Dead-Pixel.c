65cdfd9154ccee4a1111eb3bea3dbf7c5c2e8a94
StepthenFriedman <143859543+StepthenFriedman@users.noreply.github.com>
2023-11-21 11:37:34 +0800

    11/21

--- 作业与课堂练习/codeforces/Dead-Pixel.c
+++ 作业与课堂练习/codeforces/Dead-Pixel.c
@@ -3,24 +3,12 @@
 int max(int x,int y){
     return x>y?x:y;
 }
-
-//5 10 3 9
-//3 9
-
-//0 0 0 0 0 0 0 0 0 0 
-//0 0 0 0 0 0 0 0 0 0 
-//0 0 0 0 0 0 0 0 0 0 
-//0 0 0 0 0 0 0 0 0 1 
-//0 0 0 0 0 0 0 0 0 0 
-
 int main(){
     int t,i,a,b,x,y,tmp1,tmp2;
     scanf("%d",&t);
     for (i=0;i<t;i++){
         scanf("%d%d%d%d",&a,&b,&x,&y);
-        tmp1=a*max(y,b-y-1);
-        tmp2=b*max(x,a-x-1);
-        printf("%d %d %d\n",tmp1,tmp2,max(tmp1,tmp2));
+        printf("%d\n",max(a*max(y,b-y-1),b*max(x,a-x-1)));
     }
     return 0;
 }
---
+++ 作业与课堂练习/codeforces/Homecoming.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//3 2 8
+//AABBBBAABB
+
+int main(){
+    int t,n,i,j,a,b,p;
+    char road[200000];
+    scanf("%d",&t);
+    for (i=0;i<t;i++){
+        scanf("%d%d%d%s",&a,&b,&p,&road);
+        
+    }
+    return 0;
+}
